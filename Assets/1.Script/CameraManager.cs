b4dd4e338a6f3187dc2ead34374ee4ce4fa40259
adswqqe <xotjdbkdnj@naver.com>
2020-04-10 13:28:09 +0900

    카메라 스크립트 수정중

--- Assets/1.Script/CameraManager.cs
+++ Assets/1.Script/CameraManager.cs
@@ -31,7 +31,7 @@ public class CameraManager : MonoBehaviour
     public const float TOUCH_ROTATION_WIDTH = 1; // Always3
     public const float TOUCH_ROTATION_MINIMUM = 1;
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         float pinchAmount = 0;
         Quaternion desiredRotation = transform.rotation;
@@ -60,78 +60,82 @@ public class CameraManager : MonoBehaviour
                 StartPosition = GetWorldPosition();
             }
         }
-        else if (Input.touchCount == 2)
-        {
-            if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0 && Input.GetTouch(1).phase == TouchPhase.Moved)  //rotate
+        //else if (Input.touchCount == 2)
+        //{
+            //if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0 && !isZooming)  //rotate
+            // {
+
+            //두지점의 좌표값을 가져온다. 
+            var _firstPoint = Input.GetTouch(0).position;
+            var _secondPoint = Input.GetTouch(1).position;
+
+            //두점 사이의 x의 길이 y의 길이를 구한다.
+            var v2 = _firstPoint - _secondPoint;
+
+            //두 지점의 각도를 구한다. -3.14 ~ 3.14
+            var newAngle = Mathf.Atan2(v2.y, v2.x);
+
+            //180/Mathf.PI 를 곱해서 -180~180 범위로 변경한다.
+            newAngle = newAngle * 180 / Mathf.PI;
+
+            //뒤 각도에서 앞 각도빼기.
+            var deltaAngle = Mathf.DeltaAngle(newAngle, oldAngle);
+
+            //비교할 old값 셋팅
+            oldAngle = newAngle;
+
+            //첫 터치에는 newAngle 값밖에 없기때문에ㅐ deltaAngle을 구할수가 없다. 
+            //그러므로 터치가 처음시작될경우는 제외시켜주고, TouchPhase 가 Moved일 경우에 회전시켜준다. 
+            if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began || Mathf.Abs(deltaAngle) > 11)
             {
-                isRotating = true;
-
-                //두지점의 좌표값을 가져온다. 
-                var _firstPoint = Input.GetTouch(0).position;
-                var _secondPoint = Input.GetTouch(1).position;
-
-                //두점 사이의 x의 길이 y의 길이를 구한다.
-                var v2 = _firstPoint - _secondPoint;
-
-                //두 지점의 각도를 구한다. -3.14 ~ 3.14
-                var newAngle = Mathf.Atan2(v2.y, v2.x);
-
-                //180/Mathf.PI 를 곱해서 -180~180 범위로 변경한다.
-                newAngle = newAngle * 180 / Mathf.PI;
-
-                //뒤 각도에서 앞 각도빼기.
-                var deltaAngle = Mathf.DeltaAngle(newAngle, oldAngle);
-
-                //비교할 old값 셋팅
-                oldAngle = newAngle;
-
-                //첫 터치에는 newAngle 값밖에 없기때문에ㅐ deltaAngle을 구할수가 없다. 
-                //그러므로 터치가 처음시작될경우는 제외시켜주고, TouchPhase 가 Moved일 경우에 회전시켜준다. 
-                if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began || Mathf.Abs(deltaAngle) > 11)
+                debugText.text = "얘는 일 안함??";
+                return;
+            }
+            else if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0 && !isZooming/*Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved && !isZooming*/)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Stationary)
                 {
-                        debugText.text = "얘는 일 안함??";
+                    debugText.text = "해치웠나?";
                     return;
                 }
-                else if (Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved && !isZooming)
+                Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+                RaycastHit[] hits = Physics.RaycastAll(ray, 30);
+
+                Vector3 position = new Vector3();
+                foreach (RaycastHit hit in hits)
                 {
-                    if(Input.GetTouch(0).phase == TouchPhase.Stationary)
-                    {
-                        debugText.text = "해치웠나?";
-                        return;
-                    }
-                    Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-                    RaycastHit[] hits = Physics.RaycastAll(ray, 30);
-
-                    Vector3 position = new Vector3();
-                    foreach (RaycastHit hit in hits)
-                    {
-                        position = hit.transform.position;
-                    }
-                    transform.RotateAround(position, Vector3.up, -deltaAngle);
-                    debugText.text = "deltaAngle?" + deltaAngle;
+                    position = hit.transform.position;
                 }
+                transform.RotateAround(position, Vector3.up, -deltaAngle);
+                debugText.text = "deltaAngle?" + deltaAngle;
+                isRotating = true;
             }
-        }
+            //}
+            if (Mathf.Abs(DetectTouchMovement.pinchDistanceDelta) > 0 && !isRotating)
+            {
+                //if (Mathf.Abs(DetectTouchMovement.pinchDistanceDelta) > 0/*Input.GetTouch(1).phase == TouchPhase.Moved && !isRotating*/)
+                //{
+                isZooming = true;
 
-        //if (Mathf.Abs(DetectTouchMovement.pinchDistanceDelta) > 0/*Input.GetTouch(1).phase == TouchPhase.Moved && !isRotating*/)
-        //{
-        //    isZooming = true;
+                DragNewPosition = GetWorldPositionOfFinger(1);
+                Vector2 PositionDifference = DragNewPosition - DragStartPosition;
 
-        //    DragNewPosition = GetWorldPositionOfFinger(1);
-        //    Vector2 PositionDifference = DragNewPosition - DragStartPosition;
+                if (Vector2.Distance(DragNewPosition, Finger0Position) < DistanceBetweenFingers)
+                    GetComponent<Camera>().orthographicSize += (PositionDifference.magnitude);
 
-        //    if (Vector2.Distance(DragNewPosition, Finger0Position) < DistanceBetweenFingers)
-        //        GetComponent<Camera>().orthographicSize += (PositionDifference.magnitude);
+                if (Vector2.Distance(DragNewPosition, Finger0Position) >= DistanceBetweenFingers)
+                    GetComponent<Camera>().orthographicSize -= (PositionDifference.magnitude);
 
-        //    if (Vector2.Distance(DragNewPosition, Finger0Position) >= DistanceBetweenFingers)
-        //        GetComponent<Camera>().orthographicSize -= (PositionDifference.magnitude);
-
-        //    DistanceBetweenFingers = Vector2.Distance(DragNewPosition, Finger0Position);
+                DistanceBetweenFingers = Vector2.Distance(DragNewPosition, Finger0Position);
 
 
-        //}
-        //DragStartPosition = GetWorldPositionOfFinger(1);
-        //Finger0Position = GetWorldPositionOfFinger(0);
+                //}
+                DragStartPosition = GetWorldPositionOfFinger(1);
+                Finger0Position = GetWorldPositionOfFinger(0);
+            }
+      //  }
+
+
 
     }
 
